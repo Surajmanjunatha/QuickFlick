2fa1da7ca0f71be5fe7ad50cf875951037ea2957
Surajmanjunatha <surajmanju2005@gmail.com>
2025-12-18 00:42:06 +0530

    connected database and started working on backend

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+
+const app = express();
+const port = 3000;
+
+await connectDB()
+
+//middleware
+
+app.use(express.json());
+app.use(cors());
+
+
+//API routes
+app.get('/', (req, res) => res.send('server is Live!'))
+
+app.listen(port, ()=> console.log(`server listening at http://localhost:${port}`));
